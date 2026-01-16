fcb58e7c8199b820b8e8a9a3c12afa4eec6c10dc
Oshan Avishka <oshanavishka06@gmail.com>
2026-01-16 16:29:46 +0530

    feat ( Project ) : init

---
+++ server/cortex/src/test/java/com/cortexpro/cortex/CortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cortexpro.cortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
